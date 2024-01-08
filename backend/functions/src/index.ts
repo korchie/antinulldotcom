5a6451ebc815de0d38c386d270edfeed25432855
korchie <korchie44@gmail.com>
2024-01-08 13:33:09 -0500

    second commit

--- backend/functions/src/index.ts
+++ backend/functions/src/index.ts
@@ -2,11 +2,18 @@ import * as functions from "firebase-functions";
 import express from "express";
 import cors from "cors";
 import routes from "./routes/collectionRoutes";
+// import collectionRoutes from "./routes/collectionRoutes";
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
+// adding the port info
+const port = 3000;
+app.listen(port, () => console.log(`Listening on port: ${port}`));
+
 app.use("/", routes);
+// app.use("/", collectionRoutes);
 
 export const api = functions.https.onRequest(app);
--- backend/functions/src/routes/collectionRoutes.ts
+++ backend/functions/src/routes/collectionRoutes.ts
@@ -1,23 +1,22 @@
 import express from "express";
 import { getClient } from "../db";
 // import { ObjectId } from "mongodb";
-// import gameCollectionModel from "../models/gameCollectionModel";
+import {MongoCl}
+import gameCollectionInterface from "../models/gameCollectionInterface";
 
 const routes = express.Router();
 
 routes.get("/gameCollection", async (req, res) => {
   try {
     const client = await getClient();
-    const results = await client
-      .db()
-      .collection("gameCollection")
-      .find()
-      .toArray();
+    const results = await client.db().collection("gameCollection").find().toArray();
     res.json(results); // send json results
   } catch (err) {
     console.error("ERROR", err);
+    // console.log("server up and running!");
     res.status(500).json({ message: "Internal Server Error" });
   }
 });
+
 
 export default routes;
